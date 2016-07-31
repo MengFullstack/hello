b2643d5d506f792485559932e75be11b2615151e
meng <386833998@qq.com>
2016-07-31 10:47:00 +0800

    add b.c to dev

---
+++ b.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void) {
+
+    int a,b;
+    a=2;
+    b=3;
+
+    if(a>b) {
+    
+        printf("a>b\n");
+    }else {
+        
+        printf("a<=b\n");
+    }
+    return 0;
+}
