8478f9ad46cb75299b5890a356e64070b43d6441
MengFullstack <386833998@qq.com>
2016-08-11 20:03:49 +0800

    Update a.c

--- a.c
+++ a.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
-
+#include<stdlib.h>
+#include<string.h>
 int main(void) {
     
-    int a ,b;
-    a=10;
-    b=20;
-    printf("a+b=%d\n,a+b");
-  //  printf("helloworld\n");
+    int i=0;
+    for(i=0;i<100;i++) {
+        printf("rand=%d , ",rand()%100);
+    }
+    printf("\n");
     return 0;
 }
